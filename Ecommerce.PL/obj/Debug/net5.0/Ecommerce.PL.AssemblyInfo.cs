165e618da18da770a139619cd46db93e7e3a3c9f
Mostafaessam7 <mtefa2777@gmail.com>
2024-10-02 05:50:38 +0300

    Update

--- Ecommerce.PL/obj/Debug/net5.0/Ecommerce.PL.AssemblyInfo.cs
+++ Ecommerce.PL/obj/Debug/net5.0/Ecommerce.PL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.PL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f351ace38084f8b10bb48392407dcc93756ed56c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.PL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.PL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
